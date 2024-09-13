1d7715f301a10267db5c9776130f2835a60911ed
lironLRJ <roshainwillathgamuwa11@gmail.com>
2024-09-13 12:51:04 +0530

    first comit

---
+++ script.js
@@ -0,0 +1,32 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('my-cache').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/styles/styles1.css',
+          '/styles/styles2.css',
+          '/script.js',
+          '/favicon_package/android-chrome-192x192.png'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
+
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+        console.log('ServiceWorker registered with scope: ', registration.scope);
+      }).catch(function(error) {
+        console.log('ServiceWorker registration failed: ', error);
+      });
+    });
+  }
